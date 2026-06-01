57e5bf3114f9f459521b2cff72c0af56310d3ed9
azam yest tsar <akbarashurov2702@gmail.com>
2026-06-01 22:00:58 +0400

    added db

--- logic.py
+++ logic.py
@@ -2,7 +2,41 @@ import sqlite3
 
 class BotFunc:
     def __init__(self, db_name):
-        pass
+        self.db_name = db_name
+        self.conn = sqlite3.connect(self.db_name, check_same_thread=False)
+        self.conn = sqlite3.connect(self.db_name)
+        self.cursor = self.conn.cursor()
+        self.create_table()
+
+    def create_table(self):
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS users (
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                user_id INTEGER UNIQUE,
+                                username TEXT,
+                                age INTEGER,
+                                degree_yn TEXT,
+                                speciality TEXT,
+                                have_device_laptop TEXT
+                            )''')
+        self.conn.commit()
+        self.conn.close()
+
+    def register_user(self, user_id, username, age, degree_yn, speciality, have_device_laptop):
+        self.cursor.execute('''INSERT OR IGNORE INTO users 
+                               (user_id, username, age, degree_yn, speciality, have_device_laptop)
+                               VALUES (?, ?, ?, ?, ?, ?)''',
+                            (user_id, username, age, degree_yn, speciality, have_device_laptop))
+        self.conn.commit()
+        self.conn.close()
+
+
+    def Delete_user(self, user_id):
+        self.cursor.execute('''DELETE FROM users WHERE user_id = ?''', (user_id,))
+        self.conn.commit()
+        self.conn.close()
+
 
 if __name__ == "__main__":
+    bot_func = BotFunc("users.db")
+    bot_func.create_table()
     print("This is the logic module.")
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import sqlite3 
 from telebot import TeleBot
 import dotenv
+import telebot
 import os
+from logic import BotFunc
+
 
 dotenv.load_dotenv()
 bot = TeleBot(os.getenv('TOKEN'))
+
+Users = []
+bot_logic = BotFunc('users.db')
 
 
 @bot.message_handler(commands=['help'])
@@ -22,15 +28,83 @@ def help(message):
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'Welcome to the bot! Type /help to see available commands.' \
+    bot.send_message(message.chat.id, 'Welcome to the bot!\nType /help to see available commands.' \
                                       '\nTo get started, you can register an account using /register.')
+
+
+@bot.message_handler(commands=['register'])
+def registration(message):
+    global Users
+    bot.send_message(message.chat.id, 'Please enter your username:')
+    Users.append({'UserID' : message.from_user.id, 'User_Name' : None, 'Age' : None, 'degree_YN' : None, 'Speciality' : None, 'Have_Device_Laptop' : None, 'State' : 'Awaiting_Name'})
 
 @bot.message_handler(func = lambda message: True)
 def forAll(message):
-    bot.send_message(message.chat.id, 'hello, world!')
+    global Users
+    for User in Users:
+        if User['UserID'] == message.from_user.id:
 
+            if User['State'] == 'Awaiting_Name':
+                User['User_Name'] = message.text
+                User['State'] = 'Awaiting_Age'
+                bot.send_message(message.chat.id, 'Please enter your age:')
+                continue
 
-bot.infinity_polling()
+            if User['State'] == 'Awaiting_Age':
+                try:
+                    User['Age'] = int(message.text)
+                except ValueError:
+                    bot.send_message(message.chat.id, 'Invalid input for age. Please enter a valid number:')
+                    continue
+                User['State'] = 'Awaiting_Degree'
+                bot.send_message(message.chat.id, 'Do you have a degree? (Yes/No)')
+                continue
+
+            if User['State'] == 'Awaiting_Degree':
+                User['degree_YN'] = message.text
+                User['State'] = 'Awaiting_Speciality'
+                bot.send_message(message.chat.id, 'What is your speciality?')
+                continue    
+
+            if User['State'] == 'Awaiting_Speciality':
+                User['Speciality'] = message.text
+                User['State'] = 'Awaiting_Device'
+                bot.send_message(message.chat.id, 'Do you have a laptop? (Yes/No)')
+                continue   
+
+            if User['State'] == 'Awaiting_Device':
+                User['Have_Device_Laptop'] = message.text
+                User['State'] = 'Registered'
+                print(User)
+                bot_logic.register_user(user_id=User['UserID'], username=User['User_Name'], age=User['Age'], degree_yn=User['degree_YN'], speciality=User['Speciality'], have_device_laptop=User['Have_Device_Laptop'])
+                bot.send_message(message.chat.id, 'You have successfully registered!\nYour information:\n' \
+                                                      f'Username: {User["User_Name"]}\n' \
+                                                      f'Age: {User["Age"]}\n' \
+                                                      f'Degree: {User["degree_YN"]}\n' \
+                                                      f'Speciality: {User["Speciality"]}\n' \
+                                                      f'Laptop: {User["Have_Device_Laptop"]}')
+                Users.remove(User)
+                continue
+
+bot.delete_my_commands(scope=None, language_code=None)
+
+bot.set_my_commands(
+    commands= [
+        telebot.types.BotCommand("/start","start bot"),
+        telebot.types.BotCommand("/register","register user"),
+        telebot.types.BotCommand("/recommend","Reconmendation for JOBLESS"),
+        telebot.types.BotCommand("/view","view info"),
+        telebot.types.BotCommand("/delete","delete info"),
+        telebot.types.BotCommand("/update","update info"),
+        telebot.types.BotCommand("/help","help")
+            ],
+        scope=None, language_code=None
+)
+
+# check command
+cmd = bot.get_my_commands(scope=None, language_code=None)
+print([c.to_json() for c in cmd])
+
 
 if __name__ == '__main__':
-    print("Bot is running...")
+    bot.infinity_polling()
